400bcbc026c3ac532c17ba2d3654b2297b01ccd2
User <kvgoncharova_2@edu.hse.ru>
2023-03-08 21:56:16 +0300

    Getting data from two services: Cabinet MIEM and Zulip was created

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import os
 from dotenv import dotenv_values
 
-cabinet_token = dotenv_values(".env").get('cabinet_token')
+Token = dotenv_values(".env").get('token')
--- main.py
+++ main.py
@@ -1,25 +1,23 @@
 import requests
 import config
 import zulip
+import json
 
-#Данные из кабинета МИЭМа
-fl = open('C:/Users/user/PycharmProjects/api/GoncharovaKV.txt', 'w')
-response = requests.get('https://cabinet.miem.hse.ru/api/student_profile', headers={"x-auth-token": config.cabinet_token})
+# Данные из кабинета МИЭМа
+fl = open('C:/Users/user/PycharmProjects/api/MIEM.json', 'w', encoding='utf-8')
+response = requests.get('https://cabinet.miem.hse.ru/api/student_profile', headers={"x-auth-token": config.Token})
 r = response.json()
 
-name = r["data"][0]["main"]["name"]
-group = r["data"][0]["main"]["group"]
-id = r["data"][0]["main"]["studentId"]
-email = r["data"][0]["main"]["email"]
-description = r["data"][0]["main"]["description"][0]
-avatar = r["data"][0]["main"]["pic"]
-
-fl.write('Данные из кабинета МИЭМа:' + '\n')
-fl.write('ФИО:  ' + name + '\n')
-fl.write('Группа:  ' + group + '\n')
-fl.write('Статус:  ' + description + '\n')
-fl.write('Почта:  ' + email + '\n')
-fl.write('ID:  ' + str(id) + '\n')
-fl.write('URL-аватар: ' + avatar + '\n')
+fl.write(json.dumps(r, ensure_ascii=False))
 
 fl.close()
+
+# Данные из Zulip
+file = open('C:/Users/user/PycharmProjects/api/Zulip.json', 'w', encoding='utf-8')
+
+client = zulip.Client(config_file="C:/Users/user/PycharmProjects/api/api.env")
+result = client.get_profile()
+
+file.write(json.dumps(result, ensure_ascii=False))
+
+file.close()
