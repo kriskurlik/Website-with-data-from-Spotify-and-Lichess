ab28449c74bfb5ae7c3248a369bdee83df8e578d
User <kvgoncharova_2@edu.hse.ru>
2023-03-10 18:05:05 +0300

    Удалила файл .env

--- config.py
+++ config.py
@@ -1,5 +1,14 @@
+import os
 from dotenv import dotenv_values
 
 MIEM_TOKEN = dotenv_values(".env").get('MIEM_TOKEN')
+if MIEM_TOKEN is None:
+    MIEM_TOKEN = os.getenv('MIEM_TOKEN')
+
 LICHESS_TOKEN = dotenv_values(".env").get('LICHESS_TOKEN')
+if LICHESS_TOKEN is None:
+    LICHESS_TOKEN = os.getenv('LICHESS_TOKEN')
+
 LICHESS_NICK = dotenv_values(".env").get('LICHESS_NICK')
+if LICHESS_NICK is None:
+    LICHESS_NICK = os.getenv('LICHESS_NICK')
