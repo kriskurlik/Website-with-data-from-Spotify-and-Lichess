e7de5bc7fee7a71032168a8dd4fb02b217cf26df
User <kvgoncharova_2@edu.hse.ru>
2023-03-17 18:50:32 +0300

    Программа по отображению данных из трех сервисов

--- config.py
+++ config.py
@@ -12,3 +12,17 @@ if LICHESS_TOKEN is None:
 LICHESS_NICK = dotenv_values(".env").get('LICHESS_NICK')
 if LICHESS_NICK is None:
     LICHESS_NICK = os.getenv('LICHESS_NICK')
+
+ID_SPOTIFY = dotenv_values(".env").get('ID_SPOTIFY')
+if ID_SPOTIFY is None:
+    ID_SPOTIFY = os.getenv('ID_SPOTIFY')
+
+X_RapidAPI_Key = dotenv_values(".env").get('X_RapidAPI_Key')
+if X_RapidAPI_Key is None:
+    X_RapidAPI_Key = os.getenv('X_RapidAPI_Key')
+
+X_RapidAPI_Host = dotenv_values(".env").get('X_RapidAPI_Host')
+if X_RapidAPI_Host is None:
+    X_RapidAPI_Host = os.getenv('X_RapidAPI_Host')
+
+
--- main.py
+++ main.py
@@ -1,21 +1,86 @@
 """
 created by Goncharova Kristina BIV221
-Get information about person from Cabinet MIEM and Lichess
+Get information about person from Cabinet MIEM, Lichess, Spotify.
 """
 import json
 import requests
 import berserk
+from jinja2 import Template
 import config
+
 # Данные из кабинета МИЭМа
+response = requests.get('https://cabinet.miem.hse.ru/api/student_profile',
+                        headers={"x-auth-token": config.MIEM_TOKEN},
+                        timeout=10)
+r = response.json()
 with open("./MIEM.json", 'w', encoding='utf-8') as fl:
-    response = requests.get('https://cabinet.miem.hse.ru/api/student_profile',
-                            headers={"x-auth-token": config.MIEM_TOKEN},
-                            timeout=10)
-    r = response.json()
     fl.write(json.dumps(r, ensure_ascii=False))
 
 # Данные из Lichess
 session = berserk.TokenSession(config.LICHESS_TOKEN)
 client = berserk.Client(session=session)
+a = client.users.get_public_data(config.LICHESS_NICK)
 with open("./Lichess.json", "w", encoding="utf-8") as fl1:
     fl1.write(str(client.users.get_public_data(config.LICHESS_NICK)))
+
+# Данные из Спотифая
+URL = "https://spotify23.p.rapidapi.com/user_profile/"
+querystring = {"id": str(config.ID_SPOTIFY), "playlistLimit": "10", "artistLimit": "10"}
+headers = {
+    "X-RapidAPI-Key": str(config.X_RapidAPI_Key),
+    "X-RapidAPI-Host": str(config.X_RapidAPI_Host)
+}
+answer = requests.request("GET", URL, headers=headers, params=querystring)
+with open("./Spotify.json", "w", encoding="utf-8") as fl2:
+    fl2.write(answer.text)
+
+
+TEMPLATE_PATH = "index1.html"
+INDEX_PATH = "index.html"
+
+
+def create_template(TEMPLATE_PATH):
+    """
+    Функция формирует шаблон из файла
+    :param template_path:
+    :return: Template(template_html)
+    """
+    template_html = open(TEMPLATE_PATH, encoding="utf-8").read()
+    return Template(template_html)
+
+
+def get_name_of_playlist(ans):
+    """
+    Функция заполняет массив назаниями плейлистов
+    :param ans:
+    :return: names
+    """
+    names = []
+    for i in range(0, ans.json()["total_public_playlists_count"]):
+        names.append(ans.json()["public_playlists"][i]["name"])
+    return names
+
+
+if __name__ == '__main__':
+    template = create_template(TEMPLATE_PATH)
+    print(a)
+    playlist = get_name_of_playlist(answer)
+    rendered_page = template.render(
+        name=r["data"][0]["main"]["name"],
+        status=r["data"][0]["main"]["description"][0],
+        email=r["data"][0]["main"]["email"],
+        chatlink=r["data"][0]["main"]["chatLink"],
+        avatar=r["data"][0]["main"]["pic"],
+        nick_name=answer.json()["name"],
+        following_count=answer.json()["following_count"],
+        playlist_names=playlist,
+        name_li=a["username"],
+        url_li=a["url"],
+        all_games=a["count"]["all"],
+        win=a["count"]["win"],
+        loss=a["count"]["loss"]
+
+
+    )
+    with open(INDEX_PATH, "w", encoding='utf-8') as fl3:
+        fl3.write(rendered_page)
